2bd911efbcb4fdb022ac837c7463063528a1fc09
Phil Cook <phil@phil-cook.com>
2016-07-30 22:20:03 +0100

    Add travis ci testing, simply the start of messing with this.

---
+++ .travis/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+    brew tap homebrew/dupes
+    brew tap homebrew/versions
+    brew tap homebrew/homebrew-php
+    brew update
+    brew install php53
+    brew unlink php53
+    brew install php54
+    brew unlink php54
+    brew install php55
+    brew unlink php55
+    brew install php56
+    brew unlink php56
+    brew install php70
+    brew unlink php70
+    brew install php71
+    brew unlink php71
+
+    echo 'Installed all PHP versions.'
+fi
