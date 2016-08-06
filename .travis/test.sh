210f9bed57228051990c6161eb441e1789f3babe
JamesRezo <james@rezo.net>
2016-08-06 15:33:31 +0200

    Travis test tryout

---
+++ .travis/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+error=0
+for version in 53 54 55 56 70 71
+do
+    phpswitch.sh $version -s > /dev/null
+    switched=$(php -v | grep -e '^PHP' | cut -d' ' -f2 | cut -d. -f1,2 | sed 's/\.//')
+    if [ $version -ne $switched ];
+    then
+        error=1
+        echo -n 'E'
+    fi
+    echo -n '.'
+done
+echo ''
+
+exit $error
