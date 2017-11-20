586bad41abf94b59145ca765b3b8e8ab5f14d89f
morenice <hyoungguyo@hotmail.com>
2017-11-20 15:07:45 +0900

    add brew services start/stop/cleanup
    - validatation installed of 'brew services'.
    - add command 'brew services start/stop/cleanup'
    - add processing messages.
    - refactoring that reduce the if-else depth.
    - bugfix that no argument case.

--- phpswitch.sh
+++ phpswitch.sh
@@ -16,6 +16,15 @@ php7_module="php7_module"
 apache_php7_lib_path="\/libexec\/apache2\/libphp7.so"
 native_osx_php_apache_module="LoadModule php5_module libexec\/apache2\/libphp5.so"
 
+# Has the user submitted a version required
+if [[ "$#" -eq 0 ]] || [[ -z "$1" ]]
+then
+	echo "usage: brew-php-switcher version [-s]"; echo;
+	echo "    version    one of:" ${brew_array[@]};
+	echo "    -s         skip change of mod_php on apache"; echo;
+	exit
+fi
+
 php_module="$php5_module"
 apache_php_lib_path="$apache_php5_lib_path"
 if [ $(echo "$php_version" | sed 's/^php//') -ge 70 ]; then
@@ -26,15 +35,6 @@ fi
 apache_change=1
 apache_conf_path="/etc/apache2/httpd.conf"
 apache_php_mod_path="$php_opt_path$php_version$apache_php_lib_path"
-
-# Has the user submitted a version required
-if [[ -z "$1" ]]
-then
-	echo "usage: brew-php-switcher version [-s]"; echo;
-	echo "    version    one of:" ${brew_array[@]};
-	echo "    -s         skip change of mod_php on apache"; echo;
-	exit
-fi
 
 # Check for skip apache
 while [[ ${2:0:1} = '-' ]] ; do
@@ -51,70 +51,106 @@ while [[ ${2:0:1} = '-' ]] ; do
 	shift
 done
 
+echo "Switching to $php_version ..."
+echo ""
+
+# Install brew services
+brew tap | grep homebrew/services &> /dev/null
+if [ $? != 0 ];then
+    echo "Sorry, homebrew/services is not installed. "
+    echo "Install by running: brew tap homebrew/services"
+    exit 255
+fi
+
 # What versions of php are installed via brew
 for i in ${php_array[*]}
-	do
-		if [[ -n "$(brew ls --versions "$i")" ]]
-		then
-			php_installed_array+=("$i")
-		fi
+do
+    if [[ -n "$(brew ls --versions "$i")" ]]
+    then
+        php_installed_array+=("$i")
+    fi
 done
 
 # Check that the requested version is supported
-if [[ " ${php_array[*]} " == *"$php_version"* ]]
+if [[ " ${php_array[*]} " != *"$php_version"* ]]
 then
-	# Check that the requested version is installed
-	if [[ " ${php_installed_array[*]} " == *"$php_version"* ]]
-	then
-		# Switch Shell
-		echo "Switching to $php_version"
-		echo "Switching your shell"
-		for i in ${php_installed_array[@]}
-		do
-			if [[ -n $(brew ls --versions $i) ]]
-			then
-				brew unlink $i
-			fi
-		done
-		brew link "$php_version"
-
-		# Switch apache
-		if [[ $apache_change -eq 1 ]]; then
-			echo "You will need sudo power from now on"
-			echo "Switching your apache conf"
-
-			for j in ${php_installed_array[@]}
-			do
-				loop_php_module="$php5_module"
-				loop_apache_php_lib_path="$apache_php5_lib_path"
-				if [ $(echo "$j" | sed 's/^php//') -ge 70 ]; then
-					loop_php_module="$php7_module"
-					loop_apache_php_lib_path="$apache_php7_lib_path"
-				fi
-				apache_module_string="LoadModule $loop_php_module $php_opt_path$j$loop_apache_php_lib_path"
-				comment_apache_module_string="#$apache_module_string"
-
-				# If apache module string within apache conf
-				if grep -q "$apache_module_string" "$apache_conf_path"; then
-					# If apache module string not commented out already
-					if ! grep -q "$comment_apache_module_string" "$apache_conf_path"; then
-						sudo sed -i.bak "s/$apache_module_string/$comment_apache_module_string/g" $apache_conf_path
-					fi
-				# Else the string for the php module is not in the apache config then add it
-	 			else
-					sudo sed -i.bak "/$native_osx_php_apache_module/a\\
-$comment_apache_module_string\\
-" $apache_conf_path
-				fi
-			done
-			sudo sed -i.bak "s/\#LoadModule $php_module $apache_php_mod_path/LoadModule $php_module $apache_php_mod_path/g" $apache_conf_path
-			echo "Restarting apache"
-			sudo apachectl restart
-		fi
-		echo "All done!"
-	else
-		echo "Sorry, but $php_version is not installed via brew. Install by running: brew install $php_version"
-	fi
-else
-	echo "Unknown version of PHP. PHP Switcher can only handle arguments of:" ${brew_array[@]}
+	echo "Unknown version of PHP."
+    echo "PHP Switcher can only handle arguments of:" ${brew_array[@]}
+    exit 255
 fi
+
+# Check that the requested version is installed
+if [[ " ${php_installed_array[*]} " != *"$php_version"* ]]
+then
+    echo "Sorry, but $php_version is not installed via brew."
+    echo "Install by running: brew install $php_version"
+    exit 255
+fi
+
+# Switch Shell
+#
+#
+for i in ${php_installed_array[@]}
+do
+    echo "Found installed "$i
+    if [[ "$i" == "$php_version" ]]
+    then
+        brew services stop $i
+        continue
+    fi
+
+    if [[ -n $(brew ls --versions $i) ]]
+    then
+        echo "Unlink "$i"..."
+        brew services stop $i
+        brew unlink $i
+    fi
+done
+
+# Remove not used LaunchAgent configure
+echo ""
+echo "LaunchAgent cleanup..."
+brew services cleanup
+
+echo ""
+echo "Link "$i"..."
+brew link "$php_version"
+brew services start "$php_version"
+
+# Switch apache configure
+if [[ $apache_change -eq 1 ]]; then
+    echo ""
+    echo "You will need sudo power from now on"
+    echo "Switching your apache conf"
+
+    for j in ${php_installed_array[@]}
+    do
+        loop_php_module="$php5_module"
+        loop_apache_php_lib_path="$apache_php5_lib_path"
+        if [ $(echo "$j" | sed 's/^php//') -ge 70 ]; then
+            loop_php_module="$php7_module"
+            loop_apache_php_lib_path="$apache_php7_lib_path"
+        fi
+        apache_module_string="LoadModule $loop_php_module $php_opt_path$j$loop_apache_php_lib_path"
+        comment_apache_module_string="#$apache_module_string"
+
+        # If apache module string within apache conf
+        if grep -q "$apache_module_string" "$apache_conf_path"; then
+            # If apache module string not commented out already
+            if ! grep -q "$comment_apache_module_string" "$apache_conf_path"; then
+                sudo sed -i.bak "s/$apache_module_string/$comment_apache_module_string/g" $apache_conf_path
+            fi
+            # Else the string for the php module is not in the apache config then add it
+        else
+            sudo sed -i.bak "/$native_osx_php_apache_module/a\\
+            $comment_apache_module_string\\
+            " $apache_conf_path
+        fi
+    done
+    sudo sed -i.bak "s/\#LoadModule $php_module $apache_php_mod_path/LoadModule $php_module $apache_php_mod_path/g" $apache_conf_path
+    echo "Restarting apache"
+    sudo apachectl restart
+fi
+
+echo "All done!"
+exit 0
