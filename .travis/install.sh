951dda2ccb096ed43c72e51a1716d5ca2ab504b3
Miller, Andrew | Andrew | MPD <andrew.miller@rakuten.com>
2018-12-12 11:50:11 +0900

    Install initial packages via TravisCI
    To simply install script and also to help avoid possible timeout from Travis.

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
-    brew tap homebrew/dupes
-    brew update
-    brew install php@5.6
     brew unlink php@5.6
-    brew install php@7.0
     brew unlink php@7.0
-    brew install php@7.1
     brew unlink php@7.1
-    brew install php@7.2
     brew unlink php@7.2
-    brew install php@7.3
     brew link --overwrite php@7.3
     brew unlink php@7.3
 
